36342d8f152bb2973365ce6af19d9bdb650a56ed
dunnogg <gamemode61@gmail.com>
2025-02-18 23:10:06 +0300

    update redirect test

--- backend/src/shortUrl/shortUrl.controller.spec.ts
+++ backend/src/shortUrl/shortUrl.controller.spec.ts
@@ -1,28 +1,40 @@
 import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { shortUrlModule } from './shortUrl.module';
+import { shortUrlController } from './shortUrl.controller';
+import { shortUrlService } from './shortUrl.service';
 
-describe('UrlController', () => {
+describe('shortUrlController (redirect)', () => {
     let app: INestApplication;
+    let urlService;
 
     beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [shortUrlModule],
+            controllers: [shortUrlController],
+            providers: [
+                {
+                    provide: shortUrlService,
+                    useValue: {
+                        redirect: jest.fn().mockResolvedValue('https://youtube.com'), // Мокаем сервис
+                    },
+                },
+            ],
         }).compile();
 
         app = module.createNestApplication();
         await app.init();
+        urlService = module.get<shortUrlService>(shortUrlService);
     });
 
-    it('/POST shorten should create a short URL', () => {
-        return request(app.getHttpServer())
-            .post('/shorten')
-            .send({ originalUrl: 'https://youtube.com' })
-            .expect(201)
-            .expect((res) => {
-                expect(res.body).toHaveProperty('shortUrl');
-            });
+    it('GET /:shortUrl should redirect to original URL', async () => {
+        const shortUrl = 'abcd123';
+
+        const response = await request(app.getHttpServer())
+            .get(`/${shortUrl}`)
+            .expect(302);
+
+        expect(response.header.location).toBe('https://youtube.com');
+        expect(urlService.redirect).toHaveBeenCalledWith(shortUrl, expect.any(String));
     });
 
     afterAll(async () => {
